47f5f1668335f898fe0b8d02572fabf41e00f281
MfundoNtini <mfundontini@gmail.com>
2021-02-13 14:32:49 +0200

    FEATURE: Added topic create placeholder handler.

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -6,4 +6,10 @@ defmodule Discuss.TopicController do
         changeset = topic.changeset(%Topic{}, %{})
         render conn, "new.html", changeset: changeset
     end
+
+    def create(conn, params) do
+        IO.inspect(params)
+        IO.insepect(conn)
+        render conn, "create.html"
+    end
 end
