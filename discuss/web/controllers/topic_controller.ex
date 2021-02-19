703e7b96b5e9e4b1a11f9d53e75a693fe959447d
MfundoNtini <mfundontini@gmail.com>
2021-02-19 17:18:03 +0200

    FEATURE: Added topic update handler.

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -8,13 +8,27 @@ defmodule Discuss.TopicController do
     end
 
     def edit(conn, %{"id" => id }) do
-        
         case Repo.get(Topic, id) do
             :nil ->
                 render conn, "404.html", id: id
             topic_struct ->
                 changeset = Topic.changeset(topic_struct)
-                render conn, "edit.html", changeset: changeset
+                render conn, "edit.html", changeset: changeset, topic: topic_struct
+        end
+    end
+
+    def update(conn, %{"id" => id, "topic" => topic }) do
+
+        old = Repo.get(Topic, id)
+        changeset = Topic.changeset(old, topic)
+
+        case Repo.update(changeset) do
+            {:ok, post} ->
+                %Topic{title: title} = post
+                conn
+                |> put_flash(:info, "Topic `#{title}` updated.")
+                |> redirect(to: topic_path(conn, :index))
+            {:error, changeset} -> render conn, "edit.html", changeset: changeset, topic: old
         end
     end
 
@@ -27,12 +41,12 @@ defmodule Discuss.TopicController do
         changeset = Topic.changeset(%Topic{}, topic)
 
         case Repo.insert(changeset) do
-        {:ok, post} ->
-            %Topic{title: title} = post
-            conn
-            |> put_flash(:info, "Topic `#{title}` created.")
-            |> redirect(to: topic_path(conn, :index))
-        {:error, changeset} -> render conn, "new.html", changeset: changeset
+            {:ok, post} ->
+                %Topic{title: title} = post
+                conn
+                |> put_flash(:info, "Topic `#{title}` created.")
+                |> redirect(to: topic_path(conn, :index))
+            {:error, changeset} -> render conn, "new.html", changeset: changeset
         end
     end
 end
