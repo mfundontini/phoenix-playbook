59b0dc52bc9e130ee0e92c5e01e6ff5c7eb6aac5
MfundoNtini <mfundontini@gmail.com>
2021-02-25 16:08:56 +0200

    FEATURE: Insert comment into db.

--- discuss/web/channels/comments_channel.ex
+++ discuss/web/channels/comments_channel.ex
@@ -13,8 +13,18 @@ defmodule Discuss.CommentsChannel do
         {:ok, %{message: "just joined"}, socket}
     end
 
-    def handle_in(name, message, socket) do
+    def handle_in(name, %{"content" => content}, socket) do
         IO.inspect message
-        {:reply, :ok, socket}
+        topic = socket.assigns.topic
+        changeset = topic
+        |> build_assoc(:comments)
+        |> Discuss.Comment.changeset(%{content: content})
+
+        case Repo.inspect(changeset) do
+            {:ok, comment} ->
+                {:reply, :ok, socket}
+            {:error, _reason} ->
+                {:reply, {:error, %{errors: changeset}}, socket}
+        end
     end
 end
