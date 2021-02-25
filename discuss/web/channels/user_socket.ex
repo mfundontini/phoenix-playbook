b59ac8daf7de29dd71c607186be92a4344f53e42
MfundoNtini <mfundontini@gmail.com>
2021-02-26 00:38:06 +0200

    FEATURE: Extract token on connect.

--- discuss/web/channels/user_socket.ex
+++ discuss/web/channels/user_socket.ex
@@ -19,7 +19,7 @@ defmodule Discuss.UserSocket do
   #
   # See `Phoenix.Token` documentation for examples in
   # performing token verification on connect.
-  def connect(_params, socket) do
+  def connect(%{"token" => token}, socket) do
     {:ok, socket}
   end
 
