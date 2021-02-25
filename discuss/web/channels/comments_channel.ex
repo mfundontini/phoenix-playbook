e6ade04776104dce96da441292f712983f1d0eef
MfundoNtini <mfundontini@gmail.com>
2021-02-25 15:50:33 +0200

    FEATURE: Added get topic code on join.

--- discuss/web/channels/comments_channel.ex
+++ discuss/web/channels/comments_channel.ex
@@ -1,7 +1,15 @@
 defmodule Discuss.CommentsChannel do
     use Discuss.Web, :channel
 
-    def join(name, _auth, socket) do
+    def join("comments:" <> topic_id, _auth, socket) do
+        pk = String.to_integer(topic_id)
+
+        case Repo.get(Discuss.Topic, pk) do
+            :nil ->
+                {:error, %{error: "Could not join topic #{pk}"}, socket}
+            topic_struct ->
+                {:ok, %{message: "Successfully joined topic #{topic_struct.id}"}, socket}
+        end
         {:ok, %{message: "just joined"}, socket}
     end
 
